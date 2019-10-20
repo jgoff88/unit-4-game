55df79bb9dfc6030d38cf7aa2f53631d417fe5cf
James Goff <james_goff88@outlook.com>
2019-10-20 14:30:03 -0500

    update css.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -8,29 +8,35 @@ $(document).ready(function(){
     $("#crystal0").on('click', function(){
         userTotal += 2;
         console.log("your total:", userTotal);
+        $("#user").text(userTotal);
     });
     $("#crystal1").on('click', function(){
         userTotal += 5;
-        console.log("your total:", userTotal);    
+        console.log("your total:", userTotal);  
+        $("#user").text(userTotal);  
     });
     $("#crystal2").on('click', function(){
         userTotal += 6;
-        console.log("your total:", userTotal);    
+        console.log("your total:", userTotal);
+        $("#user").text(userTotal);    
     });
     $("#crystal3").on('click', function(){
         userTotal += 9;
-        console.log("your total:", userTotal);        
+        console.log("your total:", userTotal);  
+        $("#user").text(userTotal);      
     });
     $(".buttons").on("click", function() {
         if (userTotal === compTotal) {
-            alert("YOU WIN!!");
+            alert("YOU WIN!! CLICK A CRYSTAL TO TRY AGAIN.");
             wins++;
-            document.querySelector("#wins").innerHTML = wins;     
+            document.querySelector("#wins").innerHTML = wins;   
+            $("#user").text(userTotal);  
             reset();   
         } else if (userTotal > compTotal) {
-            alert("YOU LOSE! TRY AGAIN.");
+            alert("YOU LOSE! CLICK A CRYSTAL TO TRY AGAIN.");
             losses++;
             document.querySelector("#losses").innerHTML = losses;
+            $("#user").text(userTotal);
             reset();
             
         }
